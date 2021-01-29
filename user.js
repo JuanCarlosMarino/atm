d284609b9777e35bfd207d4856c92293c43eebce
Juan Carlos Marino Morantes <juan.marino.morantes@gmail.com>
2021-01-29 11:11:41 -0500

    Add user

--- user.js
+++ user.js
@@ -1,7 +1,33 @@
 class User{
-    constructor(account, password, money){
+    constructor(account, password, balance){
         this.account = 042635212;
         this.password = 123456;
-        this.money = 50000;
+        this.balance = 50000;
+    }
+    auth(account, password){
+        return account === this.account && password === this.password;
+    }
+    withdraw(amount){
+        if(this.auth() && amount < this.balance && (amount%10) === 0){
+            balance -= amount
+            return true;
+        }else{
+            return false;
+        }
+    }
+    checkBalance(){
+        if(this.auth()){
+            return this.balance;
+        }else{
+            return false;
+        }
+    }
+    consign(amount){
+        if(this.auth()){
+            this.balance += amount;
+            return true;
+        }else{
+            return false;
+        }
     }
 }
