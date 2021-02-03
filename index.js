656f50a514ae445283f1b1d8859f69bce61e1404
Juan Carlos Marino Morantes <juan.marino.morantes@gmail.com>
2021-02-02 20:34:21 -0500

    end project

--- index.js
+++ index.js
@@ -3,26 +3,42 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-const db = require('./user');
+const user = require('./user');
+const db = new user();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    //io.emit('chat message', msg);
-    console.log(msg);
-    
+  socket.on('auth', msg => {
+    if(msg.password === db.password && msg.account === db.account){
+      io.emit('auth',"ok");
+    }else{
+      io.emit('auth',"error");
+    }
+    console.log(msg)    
   });
+
   socket.on('withdraw', msg => {
-    io.emit('chat message', msg);
+    console.log(msg);
+    if(db.balance > Number(msg)){  
+      db.balance -= Number(msg);
+      io.emit('withdraw', "ok");
+    }else{                
+      io.emit('withdraw', "error");
+    }
   });
+
   socket.on('check', msg => {
-    io.emit('chat message', msg);
+    io.emit('check', db.balance);
+    console.log(msg);
   });
+
   socket.on('consign', msg => {
-    io.emit('chat message', msg);
+    console.log(msg);
+    db.balance += Number(msg);
+    io.emit('consign', "ok")
   });
 });
 
